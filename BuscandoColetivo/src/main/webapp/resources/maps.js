1a17e3966da6eaab48e7291091ff69dcafbf2823
John Kern <kern3020@gmail.com>
2012-07-17 12:27:14 -0700

    add apache 2 license to source files

--- BuscandoColetivo/src/main/webapp/resources/maps.js
+++ BuscandoColetivo/src/main/webapp/resources/maps.js
@@ -1,4 +1,21 @@
 
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 var map = null;
 
 function sleep(ms)
@@ -74,7 +91,6 @@ function getInstitutions () {
 // given a city or zip code, try to geo-reference it.
 function setupEvent() { 
 	$('#submitButton').click(function(e) {
-		alert("Go to request.");
 		e.preventDefault();
 		// e.stopPropagation();
 		// at first one might be inclined to determine if it is a zip or city.
